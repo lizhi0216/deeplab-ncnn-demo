a91a8a1ac34150737f1447efac9106b8e084e876
lizhi0216 <45022413+lizhi0216@users.noreply.github.com>
2024-03-22 13:27:25 +0800

    Add files via upload

---
+++ cmake/CMakeFiles/deeplab.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deeplab.dir/src/deeplab.cpp.o"
+  "CMakeFiles/deeplab.dir/src/deeplab.cpp.o.d"
+  "bin/deeplab"
+  "bin/deeplab.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/deeplab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
